5548a7e96ec6b09894df656121743385e4a30bb9
Srushti sodavadiya <145894270+Srushtisodavadiya@users.noreply.github.com>
2023-12-24 19:22:50 +0530

    Update array_deletion.c

--- array_deletion.c
+++ array_deletion.c
@@ -1,1 +1,25 @@
+#include<stdio.h>
+void display(int arr[],int n){
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
 
+}
+void array_deletion(int arr[],int size,int index){
+    for(int i=index;i<size-1;i++)
+    {
+        arr[i]=arr[i+1];
+    }
+}
+int main()
+{
+    int arr[100]={1,2,6,78,99};
+    int size=5, index=3;
+    display(arr,size);
+    
+    array_deletion(arr,size,index);
+    size -=1;
+    display(arr,size);
+}
