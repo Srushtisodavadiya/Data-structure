ca42bbc91f049d7238266f45faed24fb2d12dea1
Srushti sodavadiya <145894270+Srushtisodavadiya@users.noreply.github.com>
2024-02-09 22:02:13 +0530

    Update stackbyarray.c

--- stackbyarray.c
+++ stackbyarray.c
@@ -1,1 +1,47 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct stack{
+    int size;
+    int Top;
+    int * arr;
+};
+int isEmpty(struct stack *ptr){
+    if(ptr->Top==-1){
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+int isFull(struct stack *ptr){
+    if(ptr->Top==ptr->size-1){
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+int main()
+{
+   struct stack *s;
+   s->size=80;
+   s->Top=-1;
+   s->arr=(int *)malloc(s->size*sizeof(int));
+//    push operation
+    s->arr[0]=0;
+    s->Top++;
+    s->arr[1]=1;
+    s->Top++;
+    s->arr[2]=2;
+    s->Top++;
+    s->arr[3]=3;
+    s->Top++;
+   if(isEmpty(s)){
+    printf("stack is empty");
+   }
+    else{
+    printf("stack is not empty");
+    }
+   
+}
 
